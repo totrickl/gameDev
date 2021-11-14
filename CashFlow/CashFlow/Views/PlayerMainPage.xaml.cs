995efbaf58769d010edb098257c68cae280fa154
Anton Synytskyi <asynytskyi@deloitte.com>
2021-11-15 00:58:31 +0200

    update logic of player main page remove fiels from bin/obj folders

--- CashFlow/CashFlow/Views/PlayerMainPage.xaml.cs
+++ CashFlow/CashFlow/Views/PlayerMainPage.xaml.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using CashFlow.ViewModels;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 using Xamarin.Forms.Xaml;
 
 namespace CashFlow.Views
@@ -25,13 +26,20 @@ namespace CashFlow.Views
         
         private void OnEditModelButtonClicked(object sender, EventArgs e)
         {
-            if (((Button)sender).Parent is Grid grid)
-            {
-                foreach (var child in grid.Children)
-                {
-                    child.IsEnabled = true;
-                }
-            }
+            var layout = ((Button) sender).Parent.Parent;
+
+            Grid buttonsGrid = layout.FindByName<Grid>("buttons");
+            Grid fieldsGrid = layout.FindByName<Grid>("fields");
+            
+            fieldsGrid.Children.ForEach(f => f.IsEnabled = true);
+            buttonsGrid.FindByName<Button>("saveChanges").IsVisible = true;
+        }
+
+        private async void OnSaveChangesClicked(object sender, EventArgs e)
+        {
+            await App.database.SavePlayerAsync(BindingContext as PlayerViewModel);
+            ((Button) sender).Parent.Parent.FindByName<Grid>("fields").Children.ForEach(f => f.IsEnabled = false);
+            ((Button) sender).IsVisible = false;
         }
     }
 }
